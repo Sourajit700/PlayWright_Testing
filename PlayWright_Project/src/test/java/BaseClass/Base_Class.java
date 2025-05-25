adb18c108471903c2119db71a3247293aced7997
Sourajit <sourajitghosh1997@gmail.com>
2025-05-25 09:53:25 +0530

    changes in 2 part

--- PlayWright_Project/src/test/java/BaseClass/Base_Class.java
+++ PlayWright_Project/src/test/java/BaseClass/Base_Class.java
@@ -1,10 +1,14 @@
 package BaseClass;
 
+import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
 public class Base_Class {
 	public Page page;
-
+	public Browser browser;
+	public Playwright playwright;
+	
 	public Base_Class(Page page) {
 		this.page = page;
 	}
--- PlayWright_Project/target/classes/Web.features/Log_in.feature
+++
@@ -1,5 +0,0 @@
-
-Feature: Log in Feature
-@tag01
-  Scenario Outline: Log in Feature
-    Given I login to the Application "URL"
